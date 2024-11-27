71c0f5f4c881605810e03a95fb01ee043e3f5bee
HEDO <hedo@oocl.com>
2024-11-27 16:02:02 +0800

    feat: counter

---
+++ src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+const Counter = () => {
+
+    const [count, setCount] = useState(0);
+    const incrCnt = () => {
+        setCount(count + 1);
+    };
+
+    const decrCnt = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div className={"counter-item"}>
+            <button onClick={incrCnt}>+</button>
+            <span>{count}</span>
+            <button  onClick={decrCnt}>-</button>
+        </div>
+    );
+}
+
+export default Counter
